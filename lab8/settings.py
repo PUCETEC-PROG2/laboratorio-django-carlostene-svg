092c7c3a86a57de48757ece20bd246391edaabba
carlostene-svg <carlos.tene@uisek.edu.ec>
2026-01-12 21:42:07 -0500

    Terminado Laboratorio 10

--- lab8/settings.py
+++ lab8/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -45,6 +46,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -142,6 +144,11 @@ STATICFILES_DIRS =[
 LOGIN_REDIRECT_URL ="/"
 LOGOUT_REDIRECT_URL ="/"
 LOGIN_URL = "pokedex:login"
+
+CORS_ALLOW_ALL_ORIGINS = True
+#CORS_ALLOWED_ORIGINS = [
+#   "http://localhost:5173"
+#]
 
 OAUTH2_PROVIDER = {
 }
